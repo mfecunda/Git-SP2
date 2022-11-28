13d495de3d10f48febdcd0c103d78d9dbc267177
m.fecunda <m.fecunda142@GMAIL.COM>
2022-11-28 20:42:09 +0000

    update 2

--- TestGitProject/src/GitTestClass.java
+++ TestGitProject/src/GitTestClass.java
@@ -1,5 +1,12 @@
 public class GitTestClass {
     public static void main(String[] args) {
+
         System.out.println("Hellooooooooooo");
+        NewMethod();
+    }
+
+
+    public static void NewMethod (){
+        System.out.println("Updated");
     }
 }
